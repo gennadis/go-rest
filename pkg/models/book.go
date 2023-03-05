066059dc4adbd24542bcc7a370b06e60d2294a45
Gennady Badinov <gennady.badinov@gmail.com>
2023-03-05 11:46:15 +0300

    Add Book model

---
+++ pkg/models/book.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/gennadis/go-rest/pkg/config"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	Name        string `gorm:"json:name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
